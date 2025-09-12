f6b8b81026af70c953585cb4f17cb2db29d74db2
James Adeleye <73391653+olajayoflagos@users.noreply.github.com>
2025-09-12 12:16:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LandingPage from "./LandingPage";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import 'normalize.css';
 import emailjs from "emailjs-com";
